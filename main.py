6d85b157f177e0a869c8581d8af6ac195c367f45
Gizzat <gizzat@gmail.com>
2025-12-11 04:35:13 +0600

    Change layout to horizontal split; Add minimal window size

--- main.py
+++ main.py
@@ -58,17 +58,18 @@ def restore_thread():
 
 def main():
     root = tk.Tk()
+    root.minsize(338, 338)
     root.title("Star Citizen Label Highlighter")
     root.geometry(f"{WINDOW_WIDTH}x{WINDOW_HEIGHT}")
     root.configure(bg="#2E2E2E")  # Dark grey
 
     # Left frame for buttons
     button_frame = tk.Frame(root, bg="#2E2E2E")
-    button_frame.pack(side=tk.LEFT, fill=tk.Y, padx=10, pady=10)
+    button_frame.pack(side=tk.TOP, fill=tk.X, padx=10, pady=10)
 
     # Right frame for text area
     text_frame = tk.Frame(root, bg="#2E2E2E")
-    text_frame.pack(side=tk.RIGHT, expand=True, fill=tk.BOTH, padx=10, pady=10)
+    text_frame.pack(side=tk.BOTTOM, expand=True, fill=tk.BOTH, padx=10, pady=10)
 
     # Text area with scroll
     txt_area = scrolledtext.ScrolledText(text_frame, wrap='none', bg="#4A4A4A", fg="#CCCCCC")
@@ -85,16 +86,9 @@ def main():
                               width=BUTTON_WIDTH_PX // 10,
                               height=BUTTON_HEIGHT_PX // 20,
                               command=thread_highlight,
-                              bg="#4A4A4A", fg="white", activebackground="#6A6A6A", activeforeground="white")
-    btn_highlight.pack(pady=5)
-
-    btn_backup = tk.Button(button_frame,
-                           text="Backup",
-                           width=BUTTON_WIDTH_PX // 10,
-                           height=BUTTON_HEIGHT_PX // 20,
-                           command=backup_thread,
-                           bg="#4A4A4A", fg="white", activebackground="#6A6A6A", activeforeground="white")
-    btn_backup.pack(pady=5)
+                              bg="#4A4A4A", fg="white", activebackground="#6A6A6A", activeforeground="white",
+                              font=('Arial', 10, 'bold'))
+    btn_highlight.pack(side=tk.LEFT)
 
     btn_restore = tk.Button(button_frame,
                             text="Restore",
@@ -102,7 +96,15 @@ def main():
                             height=BUTTON_HEIGHT_PX // 20,
                             command=restore_thread,
                             bg="#4A4A4A", fg="white", activebackground="#6A6A6A", activeforeground="white")
-    btn_restore.pack(pady=5)
+    btn_restore.pack(side=tk.RIGHT)
+
+    btn_backup = tk.Button(button_frame,
+                           text="Backup",
+                           width=BUTTON_WIDTH_PX // 10,
+                           height=BUTTON_HEIGHT_PX // 20,
+                           command=backup_thread,
+                           bg="#4A4A4A", fg="white", activebackground="#6A6A6A", activeforeground="white")
+    btn_backup.pack(side=tk.RIGHT)
 
     root.mainloop()
 
